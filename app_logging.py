ad044a6c1f78f5e6a879c3d6faab293a3729a2f2
Jim Tyhurst <jim@tyhurst.com>
2024-12-10 04:25:37 -0800

    Uses {} style and ; delimiter for LOG_FORMAT.

--- app_logging.py
+++ app_logging.py
@@ -1,7 +1,7 @@
 import logging
 
-LOG_FORMAT = "%(asctime)s:%(levelname)s:%(name)s:%(funcName)s:%(message)s"
-LOG_FORMATTER = logging.Formatter(LOG_FORMAT)
+LOG_FORMAT = "{asctime};{levelname};{name};{funcName};{message}"
+LOG_FORMATTER = logging.Formatter(LOG_FORMAT, style="{")
 EBIRD_DASHBOARD_LOG_FILE_NAME = "ebird-dashboard.log"
 FILE_LOG_HANDLER = logging.FileHandler(
     EBIRD_DASHBOARD_LOG_FILE_NAME,
@@ -12,7 +12,10 @@ FILE_LOG_HANDLER.setFormatter(LOG_FORMATTER)
 CONSOLE_LOG_HANDLER = logging.StreamHandler()
 CONSOLE_LOG_HANDLER.setFormatter(LOG_FORMATTER)
 logging.basicConfig(
-    format=LOG_FORMAT, handlers=[CONSOLE_LOG_HANDLER, FILE_LOG_HANDLER], level=logging.WARNING
+    format=LOG_FORMAT,
+    style="{",
+    handlers=[CONSOLE_LOG_HANDLER, FILE_LOG_HANDLER],
+    level=logging.WARNING,
 )
 
 
